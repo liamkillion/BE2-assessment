2dd610109a0cc434f9355731006e02e7138e1a34
Liam Killion <liam.killion@gmail.com>
2018-02-07 12:37:23 -0500

    able to get response from server

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
-const sqlite = require('sqlite'),
-      Sequelize = require('sequelize'),
-      request = require('request'),
-      express = require('express'),
-      app = express();
+const sqlite = require("sqlite"),
+  Sequelize = require("sequelize"),
+  request = require("request"),
+  express = require("express"),
+  bodyParser = require("body-parser"),
+  app = express();
 
-const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const {
+  PORT = 3000,
+  NODE_ENV = "development",
+  DB_PATH = "./db/database.db"
+} = process.env;
 
 // START SERVER
 Promise.resolve()
-  .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
-  .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
+  .then(() =>
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
+  )
+  .catch(err => {
+    if (NODE_ENV === "development") console.error(err.stack);
+  });
 
 // ROUTES
-app.get('/films/:id/recommendations', getFilmRecommendations);
+const router = express.Router();
+
+router.get("/", function(req, res) {
+  res.json({ message: "hooray! welcome to our api!" });
+});
+app.use("/films", router);
+app.get("/films/:id/recommendations", getFilmRecommendations);
+app.post("/films", testResponse);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+  res.status(500).send("Not Implemented");
+  // res.status(200).send("All Good");
+  // const id = req.params.id;
+  // const details = { _id: new ObjectID(id) };
+  // db.collection("films").findOne(details, (err, item) => {
+  //   if (err) {
+  //     res.send({ error: "An error has occurred" });
+  //   } else {
+  //     res.send(item);
+  //   }
+  // });
+}
+
+function testResponse(req, res) {
+  res.send("Hello");
 }
 
 module.exports = app;
